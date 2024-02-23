57de2e6c6cff7d59c75addeda611e14ab03d1f22
Karshan Ahir <karshan0494@gmail.com>
2024-02-24 00:41:03 +0530

    Test4

--- src/test/java/zodiac/edi/base/ZdcMicroserviceEdiBaseLibApplicationTests.java
+++ src/test/java/zodiac/edi/base/ZdcMicroserviceEdiBaseLibApplicationTests.java
@@ -1,5 +1,8 @@
 package zodiac.edi.base;
 
+import javax.validation.constraints.AssertTrue;
+
+import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +11,7 @@ class ZdcMicroserviceEdiBaseLibApplicationTests {
 
 	@Test
 	void contextLoads() {
+		org.junit.Assert.assertEquals(true, true);
 	}
 
 }
