358d1d35f715e2a78874aa51d0d09df166f1e4f7
Karshan Ahir <karshan0494@gmail.com>
2024-02-24 23:33:51 +0530

    Test5

---
+++ src/test/java/com/orderdelivery/CalculatorTest.java
@@ -0,0 +1,11 @@
+package com.orderdelivery;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        assertEquals(true, true);
+    }
+}
